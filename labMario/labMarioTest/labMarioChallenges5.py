002b2109f70a349c7f1ee65c3ff38114ecd47838
MinjunKim <minjunkim0205@gmail.com>
2021-07-27 01:51:59 +0900

    Make the window for analysis, Code reorganization

--- labMario/labMarioTest/labMarioChallenges5.py
+++ labMario/labMarioTest/labMarioChallenges5.py
@@ -38,13 +38,7 @@ class RetroSuperMario(QWidget):
 
     # 게임 램값 케싱 업데이트
     def updateGameRamInfo(self):
-        self.emulator_map_tiles = self.emulator.get_screen()
-        # print(self.emulator_map_tiles[223][239])  # 16x16 | 224 x 240
         self.emulator_ram = self.emulator.get_ram()
-        # print("Size:" + str(self.emulator_ram.shape[0]))
-        # print("Player's state | HEX:0x000E | value:" + str(self.emulator_ram[int("0x000E", 16)]))
-        # print("Info:Player screen x | HEX:0x03AD | value:" + str(self.emulator_ram[int("0x03AD", 16)]))
-        # print("Info:Player screen y | HEX:0x00CE | value:" + str(self.emulator_ram[int("0x00CE", 16)]))
 
     # 게임 화면 업데이트
     def updateGameScreen(self):
@@ -118,26 +112,63 @@ class VisualAnalysis(QWidget):
     def __init__(self):
         super().__init__()
         # 원도우 창 정보 정의
-        self.analysis_screen_resolution_multiples = 2
-        self.analysis_screen_width = 16 * 10
-        self.analysis_screen_height = 16 * 10
+        self.analysis_screen_width = 16 * 32
+        self.analysis_screen_height = 16 * 13
         # 원도우 창 설정
         self.setFixedSize(self.analysis_screen_width, self.analysis_screen_height)
         self.analysis_screen_label = QLabel(self)
         self.analysis_screen_label.setGeometry(0, 0, self.analysis_screen_width, self.analysis_screen_height)
+        # 게임 타이머 설정 (60헤르츠)
+        self.analysis_timer = QTimer(self)
+        self.analysis_timer.timeout.connect(self.refresh)
+        self.analysis_timer.start(1000 // 60)
         # 원도우 띄우기
         self.show()
 
+    def refresh(self):
+        self.update()
+
     # 창이 업데이트 될 때마다 실행되는 함수
     def paintEvent(self, event):
+        self.full_screen_tiles_raw = game_window.emulator_ram[0x0500:0x069F + 1]
+        self.full_screen_tile_count = self.full_screen_tiles_raw.shape[0]
+        self.full_screen_page1_tile = self.full_screen_tiles_raw[:self.full_screen_tile_count // 2].reshape((13, 16))
+        self.full_screen_page2_tile = self.full_screen_tiles_raw[self.full_screen_tile_count // 2:].reshape((13, 16))
+        self.full_screen_tiles = np.concatenate((self.full_screen_page1_tile, self.full_screen_page2_tile), axis=1).astype(np.int)
+        #self.full_screen_tiles[12][31]
+        '''
+        # Empty = 0x00 - 0
+        # Fake = 0x01 - 1
+        # Ground = 0x54 - 84
+        # Top_Pipe1 = 0x12 - 18
+        # Top_Pipe2 = 0x13 - 19
+        # Bottom_Pipe1 = 0x14 - 20
+        # Bottom_Pipe2 = 0x15 - 21
+        # Flagpole_Top =  0x24 - 36
+        # Flagpole = 0x25 - 37
+        # Coin_Block1 = 0xC0 - 192
+        # Coin_Block2 = 0xC1 - 193
+        # Coin = 0xC2 - 194
+        # Breakable_Block = 0x51 - 81
+        '''
         # 그래픽 설정
-        graphic = QPainter()
-        graphic.begin(self)
-        graphic.setPen(QPen(QColor.fromRgb(255, 255, 255), 0.1, Qt.SolidLine))
-        graphic.setBrush(QBrush(Qt.blue))
-        # 직사각형 그리기
-        graphic.drawRect(0, 0, 16, 16)
-        graphic.end()
+        self.graphic = QPainter()
+        self.graphic.begin(self)
+        self.graphic.setPen(QPen(QColor.fromRgb(0, 0, 0), 0.1, Qt.SolidLine))
+        # 픽셀화 맵 그리기
+        for i in range(13):
+            for j in range(32):
+                # 빈공간 혹은 코인은 흰색으로 출력
+                if self.full_screen_tiles[i][j] == 0 or self.full_screen_tiles[i][j] == 194:
+                    self.graphic.setBrush(QBrush(QColor.fromRgb(255, 255, 255)))
+                # 도착 깃발은 초록색으로 출력
+                elif self.full_screen_tiles[i][j] == 36 or self.full_screen_tiles[i][j] == 37:
+                    self.graphic.setBrush(QBrush(QColor.fromRgb(178, 230, 25)))
+                # 나머지 블럭들은 모두 갈색
+                else:
+                    self.graphic.setBrush(QBrush(QColor.fromRgb(150, 75, 0)))
+                self.graphic.drawRect(16*j, 16*i, 16, 16)
+        self.graphic.end()
 
 # 메인
 if __name__ == '__main__':

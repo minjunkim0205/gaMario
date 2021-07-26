87ab7c16080e3534799c7c75c4a5c7cec572d52f
MinjunKim <minjunkim0205@gmail.com>
2021-07-26 23:44:11 +0900

    Code reorganization

--- labMario/labMarioTest/labMarioChallenges5.py
+++ labMario/labMarioTest/labMarioChallenges5.py
@@ -1,11 +1,11 @@
-# 01. playRetroMario.py
-# Play Super Mario Bros
+# 05. labMarioChallenges5.py
+# 화면 정보 램값을 가져와서 시각 분석 창 만들기
 
 import sys
 import retro
 import numpy as np
 from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtGui import QImage, QPixmap, QPainter, QPen, QBrush, QColor
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 
 class RetroSuperMario(QWidget):
@@ -13,30 +13,40 @@ class RetroSuperMario(QWidget):
     def __init__(self):
         super().__init__()
         self.key_state = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])  # [B, NULL, SELECT, START, U, D, L, R, A]
-        # Set retro game
+        # 레트로 모듈 설정
         self.emulator = retro.make(game='SuperMarioBros-Nes', state='Level1-1')
         self.emulator.reset()
-        # Define screen info
+        # 게임 화면 정보 정의
         self.game_screen_resolution_multiples = 2
         self.game_screen = self.emulator.get_screen()
         self.game_screen_width = self.game_screen.shape[0] * self.game_screen_resolution_multiples
         self.game_screen_height = self.game_screen.shape[1] * self.game_screen_resolution_multiples
-        # Set window screen
+        # 원도우 창 설정
         self.setFixedSize(self.game_screen_width, self.game_screen_height)
         self.game_screen_label = QLabel(self)
         self.game_screen_label.setGeometry(0, 0, self.game_screen_width, self.game_screen_height)
-        # Set game timer (60 fps)
+        # 게임 타이머 설정 (60헤르츠)
         self.game_timer = QTimer(self)
         self.game_timer.timeout.connect(self.refresh)
         self.game_timer.start(1000 // 60)
-        # Window show
+        # 원도우 띄우기
         self.show()
 
-    # Game key state update
+    # 게임 키 상태 업데이트
     def updateGameKeyState(self):
         self.emulator.step(self.key_state)
 
-    # Game screen update
+    # 게임 램값 케싱 업데이트
+    def updateGameRamInfo(self):
+        self.emulator_map_tiles = self.emulator.get_screen()
+        # print(self.emulator_map_tiles[223][239])  # 16x16 | 224 x 240
+        self.emulator_ram = self.emulator.get_ram()
+        # print("Size:" + str(self.emulator_ram.shape[0]))
+        # print("Player's state | HEX:0x000E | value:" + str(self.emulator_ram[int("0x000E", 16)]))
+        # print("Info:Player screen x | HEX:0x03AD | value:" + str(self.emulator_ram[int("0x03AD", 16)]))
+        # print("Info:Player screen y | HEX:0x00CE | value:" + str(self.emulator_ram[int("0x00CE", 16)]))
+
+    # 게임 화면 업데이트
     def updateGameScreen(self):
         self.game_screen = self.emulator.get_screen()
         self.game_screen_qimage = QImage(self.game_screen, self.game_screen.shape[1], self.game_screen.shape[0], QImage.Format_RGB888)
@@ -44,15 +54,16 @@ class RetroSuperMario(QWidget):
         self.pixel_map = self.pixel_map.scaled(self.game_screen_width, self.game_screen_height, Qt.IgnoreAspectRatio)
         self.game_screen_label.setPixmap(self.pixel_map)
 
-    # Refresh 60fps
+    # 60 프레임으로 리프레시
     def refresh(self):
         self.updateGameKeyState()
         self.updateGameScreen()
+        self.updateGameRamInfo()
 
-    # Key press
+    # 키 누르기
     def keyPressEvent(self, event):
         self.key = event.key()
-        # Move
+        # 이동
         if self.key == 68:  # R
             self.key_state[7] = 1
         if self.key == 65:  # L
@@ -61,26 +72,26 @@ class RetroSuperMario(QWidget):
             self.key_state[5] = 1
         if self.key == 87:  # U
             self.key_state[4] = 1
-        # Skill
+        # 스킬
         if self.key == 75:  # A
             self.key_state[0] = 1
         if self.key == 76:  # B
             self.key_state[8] = 1
-        # Ui control
+        # 선택화면 컨트롤 키
         if self.key == 44:  # SELECT
             self.key_state[2] = 1
         if self.key == 46:  # START
             self.key_state[3] = 1
         if self.key == 47:  # NULL
             self.key_state[1] = 1
-        # Debug Key
+        # 디버깅용 키
         if self.key == 82:
             self.emulator.reset()
 
-    # Key release
+    # 키 때기
     def keyReleaseEvent(self, event):
         self.key = event.key()
-        # Move
+        # 이동
         if self.key == 68:  # R
             self.key_state[7] = 0
         if self.key == 65:  # L
@@ -89,12 +100,12 @@ class RetroSuperMario(QWidget):
             self.key_state[5] = 0
         if self.key == 87:  # U
             self.key_state[4] = 0
-        # Skill
+        # 스킬
         if self.key == 75:  # A
             self.key_state[0] = 0
         if self.key == 76:  # B
             self.key_state[8] = 0
-        # Ui control
+        # 선택화면 컨트롤 키
         if self.key == 44:  # SELECT
             self.key_state[2] = 0
         if self.key == 46:  # START
@@ -102,9 +113,35 @@ class RetroSuperMario(QWidget):
         if self.key == 47:  # NULL
             self.key_state[1] = 0
 
-# Main
+class VisualAnalysis(QWidget):
+    # init
+    def __init__(self):
+        super().__init__()
+        # 원도우 창 정보 정의
+        self.analysis_screen_resolution_multiples = 2
+        self.analysis_screen_width = 16 * 10
+        self.analysis_screen_height = 16 * 10
+        # 원도우 창 설정
+        self.setFixedSize(self.analysis_screen_width, self.analysis_screen_height)
+        self.analysis_screen_label = QLabel(self)
+        self.analysis_screen_label.setGeometry(0, 0, self.analysis_screen_width, self.analysis_screen_height)
+        # 원도우 띄우기
+        self.show()
+
+    # 창이 업데이트 될 때마다 실행되는 함수
+    def paintEvent(self, event):
+        # 그래픽 설정
+        graphic = QPainter()
+        graphic.begin(self)
+        graphic.setPen(QPen(QColor.fromRgb(255, 255, 255), 0.1, Qt.SolidLine))
+        graphic.setBrush(QBrush(Qt.blue))
+        # 직사각형 그리기
+        graphic.drawRect(0, 0, 16, 16)
+        graphic.end()
+
+# 메인
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     game_window = RetroSuperMario()
+    analysis_window = VisualAnalysis()
     sys.exit(app.exec_())
-
